2d5718ef848f7714ae01101f62e57dd8c2dbbf9f
Heitor <heitor.cardoso39@gmail.com>
2022-09-06 15:11:25 -0300

    feat: entity types created

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,15 @@ import { AppError } from "./erros/appError";
 const app = express();
 app.use(express.json());
 
-
 // app.get("/test", (req: Request, res: Response) => {
-//     return res.status(200).json({message: "hello world!"});
-// })
+// 	return res.status(200).json({ message: "hello world!" });
+// });
 
 // app.get("/error", (req: Request, res: Response) => {
-//     throw new AppError(400, "Error working");
-// })
+// 	throw new AppError(400, "Error working");
+// });
 
 app.use(appRoutes);
 app.use(errorHandlingMiddleware);
-
 
 export { app };
---
+++ src/entities/types.entities.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity()
+export class Types {
+	@PrimaryColumn("uuid")
+	readonly id: string;
+
+	@Column({ unique: true })
+	name: string;
+
+	@Column({ nullable: true })
+	description: string;
+
+	@Column({ default: false })
+	isActive: boolean;
+
+	constructor() {
+		if (!this.id) {
+			this.id = uuid();
+		}
+	}
+}
