457332e2ac4c8c37d68e0e5b17ce3445dcdc35b1
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-09 15:45:10 -0300

    feat: attaches delete types controller to routes

--- src/routes/types.route.ts
+++ src/routes/types.route.ts
@@ -1,10 +1,11 @@
-import { createTypesController } from "../controllers/types.controllers";
+import { createTypesController, deleteTypesController } from "../controllers/types.controllers";
 import { listTypesController } from "../controllers/types.controllers";
 import { Express } from "express";
 
 const typesRoutes = (app: Express) => {
     app.post("/types", createTypesController);
     app.get("/types", listTypesController);
+    app.delete("/types/:id", deleteTypesController);
 }
 
 export { typesRoutes };
