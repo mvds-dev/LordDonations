42a2ffbdc18445b13aaf1a5162beba7636044873
Bruno Gomes <gomesbruno030@outlook.com>
2022-09-09 14:43:33 -0300

    feat-routeListAdresses

--- src/controllers/adresses.controllers.ts
+++ src/controllers/adresses.controllers.ts
@@ -1,27 +1,26 @@
 import { Request, Response } from 'express'
 import { AppError} from '../erros/appError'
 import "express-async-errors"
+
 import adressesCreateService from '../services/adresses/createAdresses.service'
+import listAdressesService from '../services/adresses/listAdresses.service'
 
 
-const adressesControler = async (req: Request, res: Response) => {
+export const adressesControler = async (req: Request, res: Response) => {
 
-    try {
+    const {city, state, number, cep, district} = req.body
 
-        const {city, state, number, cep, district} = req.body
+    const newAdresses =  await adressesCreateService({city, state, number, cep, district})
     
-        const newUser =  await adressesCreateService({city, state, number, cep, district})
-        
-        return res.status(201).send(newUser)
-
-    } catch (err) {
-
-        if (err instanceof AppError) {
-            throw new AppError(400, "erro")
-        }
-    }
-
+    return res.status(201).send(newAdresses)
 
 }
 
-export default adressesControler
+
+export const listAdressesControler = async (req: Request, res: Response) => {
+
+    const listAdresses =  await listAdressesService()
+    
+    return res.status(201).send(listAdresses)
+
+}
