1f9cea09c5f8ba6244447411734ae9326d9947fd
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-09 17:24:48 -0300

    refactor: adding comments to update institutions

--- src/services/institutions/institutionUpdate.service.ts
+++ src/services/institutions/institutionUpdate.service.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "../../data-source";
 import { Institutions } from "../../entities/institution.entity";
 import { AppError } from "../../erros/appError";
 
+//Isso aqui precisa ser arrumado
 const institutionUpdateService = async ({ id, name, email, cnpj }: any) => {
 	const institutionsRepository = AppDataSource.getRepository(Institutions);
 	const institutions = await institutionsRepository.find();
