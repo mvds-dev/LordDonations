9c996aafac626ce94173b1e6a29f88d81eb9f993
mvds <min705555@gmail.com>
2022-09-09 11:07:24 -0300

    feat:Database url

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,17 +2,14 @@ import { DataSource } from "typeorm";
 import "dotenv/config";
 
 const AppDataSource = new DataSource({
-    type:"postgres",
-    host:"localhost",
-    port:5432,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    entities: ["src/entities/**.ts"],
-    migrations: ["src/migrations/**.ts"],
-    synchronize: false,
-    logging: true
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  url: process.env.DATABASE_URL,
+  entities: ["src/entities/**.ts"],
+  migrations: ["src/migrations/**.ts"],
+  synchronize: false,
+  logging: true,
 });
 
-
-export {AppDataSource};
+export { AppDataSource };
