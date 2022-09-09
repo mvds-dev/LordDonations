f3204f5e95967943f442b9534c7a570001f30e8a
Bruno Gomes <gomesbruno030@outlook.com>
2022-09-09 15:43:35 -0300

    featAddRouteAddressesDelete

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Express } from "express";
 
-import { adressesRoutes } from "./adresses.routes";
+import { adressesRoutes } from "./addresses.routes";
 import { userRoutes } from "./user.routes";
 import { institutionsRoutes } from "./institutions.routes";
 import { typesRoutes } from "./types.route";
