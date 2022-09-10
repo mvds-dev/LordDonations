fb59fbfd83e41d88b5f4301dabaa3201f30aaea4
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-10 14:43:53 -0300

    feat: attaches delete status controller to routes

--- src/routes/statuses.routes.ts
+++ src/routes/statuses.routes.ts
@@ -1,9 +1,11 @@
 import { createStatusesController, listStatusesController } from "../controllers/statuses.controllers";
 import { Express } from "express";
+import { deleteStatusesController } from "../controllers/statuses.controllers";
 
 const statusesRoutes = (app: Express) => {
     app.post("/statuses", createStatusesController);
     app.get("/statuses", listStatusesController);
+    app.delete("/statuses/:id", deleteStatusesController);
 }
 
 export { statusesRoutes };
