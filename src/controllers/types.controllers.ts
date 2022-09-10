d17b87b42ec14e584778a228d732096cc3c46474
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-10 10:53:50 -0300

    feat: adds update types controller

--- src/controllers/types.controllers.ts
+++ src/controllers/types.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { createTypesService } from "../services/types/createTypes.service";
 import { listTypesService } from "../services/types/listTypes.service";
 import { deleteTypesService } from "../services/types/deleteTypes.service";
+import { updateTypesService } from "../services/types/updateTypes.service";
 
 
 const createTypesController = async (req: Request, res: Response) => {
@@ -21,4 +22,11 @@ const deleteTypesController = async (req: Request, res: Response) => {
     return res.status(204).send();
 }
 
-export { createTypesController, listTypesController, deleteTypesController }
+const updateTypesController = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const {name, description} = req.body;
+    const output = await updateTypesService({id, name, description});
+    return res.status(200).json(output);
+}
+
+export { createTypesController, listTypesController, deleteTypesController, updateTypesController }
