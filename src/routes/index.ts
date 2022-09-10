75b8cf23a781c1992d140e44f708cc1ded97f889
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-10 11:24:53 -0300

    feat: attaches create statuses controller to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import { userRoutes } from "./user.routes";
 import { institutionsRoutes } from "./institutions.routes";
 import { typesRoutes } from "./types.route";
 import { objectsRoutes } from "./objects.routes";
+import { statusesRoutes } from "./statuses.routes";
 
 function appRoutes(app: Express) {
     userRoutes(app)
@@ -12,6 +13,7 @@ function appRoutes(app: Express) {
     institutionsRoutes(app);
     adressesRoutes(app);
     objectsRoutes(app);
+    statusesRoutes(app);
 };
 
 export { appRoutes };
