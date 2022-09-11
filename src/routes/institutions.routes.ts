2572bbd0f69f72b5ed3bf1469b8baf27d1870428
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-11 18:14:44 -0300

    refactor: reorganizes routes distribution

--- src/routes/institutions.routes.ts
+++ src/routes/institutions.routes.ts
@@ -6,20 +6,15 @@ import {
 	institutionUpdateController,
 } from "../controllers/institutions.controllers";
 import { createRequestsController, deleteRequestsController, updateRequestsController } from "../controllers/requests.controllers";
+import { verifyInstitutionAuthMiddleware } from "../middlewares/verifyInstitutionAuth.middleware";
 import { Express } from "express";
 
 const institutionsRoutes = (app: Express) => {
 	app.post("/institutions", institutionCreateController);
 	app.get("/institutions", institutionsListController);
-	app.delete("/institutions/:id", institutionDeleteController);
+	app.delete("/institutions", verifyInstitutionAuthMiddleware, institutionDeleteController);
 	app.post("/institutions/login", institutionLoginController);
-	app.patch("/institutions/:id", institutionUpdateController);
-
-	//change this to use token
-	//requests
-	app.post("/institutions/:id/requests", createRequestsController);
-	app.delete("/institutions/:institutionId/requests/:requestId", deleteRequestsController);
-	app.patch("/institutions/:institutionId/requests/:requestId", updateRequestsController);
+	app.patch("/institutions", verifyInstitutionAuthMiddleware, institutionUpdateController);
 };
 
 export { institutionsRoutes };
--- src/routes/requests.routes.ts
+++ src/routes/requests.routes.ts
@@ -1,8 +1,14 @@
 import { listRequestsController } from "../controllers/requests.controllers";
+import { createRequestsController } from "../controllers/requests.controllers";
+import { deleteRequestsController } from "../controllers/requests.controllers";
+import { updateRequestsController } from "../controllers/requests.controllers";
 import { Express } from "express";
 
 const requestsRoutes = (app: Express) => {
     app.get("/requests", listRequestsController);
+    app.post("/requests", createRequestsController);
+	app.delete("/requests/:requestId", deleteRequestsController);
+	app.patch("/requests/:requestId", updateRequestsController);
 }
 
 export { requestsRoutes };
