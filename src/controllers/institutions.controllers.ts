a75c59111b390bdb224da0db8d6ed7ad43893f68
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-11 18:20:14 -0300

    refactor: gets institution id from token

--- src/controllers/institutions.controllers.ts
+++ src/controllers/institutions.controllers.ts
@@ -5,6 +5,8 @@ import institutionsCreateService from "../services/institutions/institutionsCrea
 import institutionsListService from "../services/institutions/institutionsList.service";
 import institutionUpdateService from "../services/institutions/institutionUpdate.service";
 
+import jwt from "jsonwebtoken";
+
 export const institutionCreateController = async (
 	req: Request,
 	res: Response,
@@ -29,8 +31,11 @@ export const institutionDeleteController = async (
 	req: Request,
 	res: Response,
 ) => {
-	const { id } = req.params;
-	const institutionDeleted = await institutionDeleteService(id);
+	const { authorization } = req.headers;
+    const token = authorization!.split(" ")[1];
+    const { id } = jwt.decode(token) as {id: string};
+
+	await institutionDeleteService(id);
 
 	return res.status(202).json({ message: "Institution deleted with success!" });
 };
@@ -50,7 +55,11 @@ export const institutionUpdateController = async (
 	res: Response,
 ) => {
 	const data = req.body;
-	data.id = req.params.id;
+
+	const { authorization } = req.headers;
+    const token = authorization!.split(" ")[1];
+    const { id } = jwt.decode(token) as {id: string};
+	data.id = id;
 
 	const updatedInstitution = await institutionUpdateService(data);
 
