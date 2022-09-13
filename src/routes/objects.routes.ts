f728772aa13cf9d13594a50ece17d87183a46f51
Heitor <heitor.cardoso39@gmail.com>
2022-09-13 01:56:37 -0300

    feat: route get by typeId and Status active

--- src/routes/objects.routes.ts
+++ src/routes/objects.routes.ts
@@ -1,16 +1,27 @@
-import { Express } from 'express'
-import { listObjectsControler } from "../controllers/objects.controllers";
-import { verifyUserAuthMiddleware } from '../middlewares/verifyUserAuth.middleware';
-import { deleteObjectsController } from '../controllers/objects.controllers';
-import { updateObjectsController } from '../controllers/objects.controllers';
-import { createObjectsController } from '../controllers/objects.controllers';
+import { Express } from "express";
+import {
+	getObjectsByTypeController,
+	listObjectsControler,
+} from "../controllers/objects.controllers";
+import { verifyUserAuthMiddleware } from "../middlewares/verifyUserAuth.middleware";
+import { deleteObjectsController } from "../controllers/objects.controllers";
+import { updateObjectsController } from "../controllers/objects.controllers";
+import { createObjectsController } from "../controllers/objects.controllers";
 
-const objectsRoutes = (app:Express) => {
-    
-    app.get('/objects',listObjectsControler);
-    app.delete("/objects/:objectId", verifyUserAuthMiddleware,deleteObjectsController);
-    app.post("/objects", verifyUserAuthMiddleware, createObjectsController);
-    app.patch("/objects/:objectId", verifyUserAuthMiddleware, updateObjectsController);
-}
+const objectsRoutes = (app: Express) => {
+	app.get("/objects", listObjectsControler);
+	app.delete(
+		"/objects/:objectId",
+		verifyUserAuthMiddleware,
+		deleteObjectsController,
+	);
+	app.post("/objects", verifyUserAuthMiddleware, createObjectsController);
+	app.patch(
+		"/objects/:objectId",
+		verifyUserAuthMiddleware,
+		updateObjectsController,
+	);
+	app.get("/objects/active/:typeId", getObjectsByTypeController);
+};
 
-export { objectsRoutes }
+export { objectsRoutes };
