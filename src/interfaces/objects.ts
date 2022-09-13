3357a0e4c9e68285b2b5d74be5f1c5872a2ab2df
Heitor <heitor.cardoso39@gmail.com>
2022-09-13 01:55:52 -0300

    fix: statusId created

--- src/interfaces/objects.ts
+++ src/interfaces/objects.ts
@@ -1,14 +1,15 @@
 export interface ICreateObjects {
-    userId: string,
-    typeId: string,
-    name: string,
-    description: string
+	userId: string;
+	typeId: string;
+	name: string;
+	description: string;
+	statusId: string;
 }
 
 export interface IUpdateObjects {
-    userId: string,
-    objectId: string,
-    typeId?: string,
-    name?: string,
-    description?: string
+	userId: string;
+	objectId: string;
+	typeId?: string;
+	name?: string;
+	description?: string;
 }
