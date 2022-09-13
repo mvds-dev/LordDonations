004015b2f2c72cc58a8f8c28a7c19e8f7c00052e
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-13 15:10:25 -0300

    feat: adds donations controllers

--- src/routes/donations.routes.ts
+++ src/routes/donations.routes.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express'
-import { listDonationsControler } from "../controllers/donations.controllers";
+import { donateToInstitutionController, listDonationsControler } from "../controllers/donations.controllers";
+import { verifyUserAuthMiddleware } from '../middlewares/verifyUserAuth.middleware';
 
 const donationsRoutes = (app:Express) => {
     
     app.get('/donations/donated',listDonationsControler);
+    app.post("/donations/:institutionId", verifyUserAuthMiddleware, donateToInstitutionController);
 }
 
 export { donationsRoutes }
