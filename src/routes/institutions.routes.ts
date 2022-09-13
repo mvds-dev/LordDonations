ca2efde5c26b6e6c5811507ee648b021352c93f0
Heitor <heitor.cardoso39@gmail.com>
2022-09-13 15:20:02 -0300

    feat: new route /institutions/donations/donated

--- src/routes/institutions.routes.ts
+++ src/routes/institutions.routes.ts
@@ -4,17 +4,30 @@ import {
 	institutionLoginController,
 	institutionsListController,
 	institutionUpdateController,
+	listDonatedObjectsController,
 } from "../controllers/institutions.controllers";
-import { createRequestsController, deleteRequestsController, updateRequestsController } from "../controllers/requests.controllers";
 import { verifyInstitutionAuthMiddleware } from "../middlewares/verifyInstitutionAuth.middleware";
 import { Express } from "express";
 
 const institutionsRoutes = (app: Express) => {
 	app.post("/institutions", institutionCreateController);
 	app.get("/institutions", institutionsListController);
-	app.delete("/institutions", verifyInstitutionAuthMiddleware, institutionDeleteController);
+	app.delete(
+		"/institutions",
+		verifyInstitutionAuthMiddleware,
+		institutionDeleteController,
+	);
 	app.post("/institutions/login", institutionLoginController);
-	app.patch("/institutions", verifyInstitutionAuthMiddleware, institutionUpdateController);
+	app.patch(
+		"/institutions",
+		verifyInstitutionAuthMiddleware,
+		institutionUpdateController,
+	);
+	app.get(
+		"/institutions/donations/donated",
+		verifyInstitutionAuthMiddleware,
+		listDonatedObjectsController,
+	);
 };
 
 export { institutionsRoutes };
