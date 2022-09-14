3ded99b07e0e0b20f5060837d974deeee8491cfc
Fernando Scramignon <scramignonnarde@gmail.com>
2022-09-14 16:01:58 -0300

    fix: uses token on patch address

--- src/routes/addresses.routes.ts
+++ src/routes/addresses.routes.ts
@@ -1,13 +1,14 @@
 import { Express } from 'express'
 import { createAdressesControler, listAdressesControler, deleteAdressesControler, updateAdressesControler } from "../controllers/adresses.controllers";
+import { verifyInstitutionAuthMiddleware } from '../middlewares/verifyInstitutionAuth.middleware';
 
 
 const adressesRoutes = (app:Express) => {
     
     app.post('/addresses',createAdressesControler)
     app.get('/addresses',listAdressesControler)
-    app.delete('/addresses/:id',deleteAdressesControler)
-    app.patch('/addresses/:id',updateAdressesControler)
+    app.delete('/addresses/:id', verifyInstitutionAuthMiddleware, deleteAdressesControler)
+    app.patch('/addresses/:id', updateAdressesControler)
     
 }
 
